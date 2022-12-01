21da96fd4796bd05cb9a6ab8ebe407f033f81fbc
Sohomdeep <sohomdeeppaul@gmail.com>
2022-12-01 20:17:07 +0530

    ses intregation and mail send

--- handler.js
+++ handler.js
@@ -1,15 +1,43 @@
 'use strict';
+const AWS = require('aws-sdk');
+const ses = new AWS.SES();
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
+module.exports.createContact = async (event, context) => {
+  console.log("received::", event);
+  const {to, from, subject, message} = JSON.parse(event.body);
+
+  if(!to || !from || !subject || !message){
+    return {
+      statusCode: 400,
+      body: JSON.stringify({message: "Data mismatch"})
+    }
+  }
+
+  const params = {
+    Destination: {
+      ToAddresses: [to]
+    },
+    Message: {
+      Body: {
+        Text: {Data: message}
       },
-      null,
-      2
-    ),
-  };
+      Subject: {Data: subject}
+    },
+    Source: from
+  }
+
+  try{
+    await ses.sendEmail(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({message: "Email sent successfully", success: true})
+    }
+  }catch(error){
+    console.error(error);
+
+    return {
+      statusCode: 400,
+      body: JSON.stringify({message: "Email faild to send", success: false})
+    }
+  }
 };
